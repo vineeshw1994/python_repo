a6eabd1b349163e8dc3cf4f2b21c719701ee564f
vineesh <vineeshw1994@gmail.com>
2026-02-01 22:08:04 +0530

    learn dymentions of arrays and indexing

--- numby/example_version.py
+++ numby/example_version.py
@@ -1,3 +1,18 @@
+# import numpy as np
+
+# print(np.__version__)
+
+
+# import numpy as np
+
+# arr = np.array([1, 2, 3, 4, 5])
+
+# print(arr)
+
+# print(type(arr))
+
 import numpy as np
 
-print(np.__version__)
+arr = np.array(42)
+
+print(arr)
---
+++ numby/indexing.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+twoD_arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+
+print('2nd element on 1st row: ', twoD_arr[0, 1])
+
+
+
+
+
+threeD_arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+
+print(threeD_arr[0, 1, 2])
